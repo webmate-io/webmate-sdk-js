5a446c986e60582a25888b9ca4ad07c4fb2c7f91
skappler <kappler_sebastian@gmx.net>
2020-04-30 08:23:30 +0200

    fix import

--- src/testmgmt/testmgmt-client.ts
+++ src/testmgmt/testmgmt-client.ts
@@ -4,10 +4,9 @@ import {WebmateAuthInfo} from "../webmate-auth-info";
 import {WebmateEnvironment} from "../webmate-environment";
 import {ProjectId, TestResultId, TestRunId} from "../types";
 import {List, Map} from "immutable";
-import {Test, TestId, TestResult} from "./testmgmt-types";
+import {Test, TestId, TestInfo, TestResult} from "./testmgmt-types";
 import {Observable, from as observableFrom} from "rxjs";
 import {map} from "rxjs/operators";
-import {TestInfo} from "../../dist/src/testmgmt/testmgmt-types";
 
 /**
  * Facade of the webmate TestMgmt subsystem, which provides access to information about Tests, TestRuns and
