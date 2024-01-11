5a467cbedd9f0ba4973f0415360526f536a8817a
Daniel Kalak <dkalak@testfabrik.com>
2024-01-11 15:38:25 +0100

    WMKNBN-8805: Adopt blocking calls from Java SDK

--- src/testmgmt/test-run.ts
+++ src/testmgmt/test-run.ts
@@ -3,17 +3,11 @@ import {TestRunId} from "../types";
 import {concat, Observable, of, throwError} from "rxjs";
 import {TestRunInfo} from "./test-run-info";
 import {TestRunEvaluationStatus} from "./test-run-evaluation-status";
-import {delay, map, mergeMap, retryWhen, take} from "rxjs/operators";
-import {TestRunExecutionStatus} from "./test-run-execution-status";
 
 /**
  * Facade for a (running or finished) TestRun
  */
 export class TestRun {
-
-    private readonly MAX_WAITING_TIME_MILLIS: number = 300_000; // 300 seconds
-    private readonly WAITING_POLLINTERVAL_MILLIS: number = 3_000; // 3 seconds
-    private readonly MAX_RETRIES: number = this.MAX_WAITING_TIME_MILLIS / this.WAITING_POLLINTERVAL_MILLIS;
 
     constructor(public readonly id: TestRunId, private readonly session: WebmateAPISession) {}
 
@@ -34,32 +28,28 @@ export class TestRun {
     }
 
     /**
-     * Finish TestRun.
+     * Finish the test run.
+     * This method is blocking:
+     * it internally calls the {@link waitForCompletion} method
+     * to wait until the test run is finished.
+     * If the test run does not finish before the timeout, the returned observable will go into an error state.
      *
-     * @param msg Short message explaining the result of the test run.
-     * @param detail Detailed information, e.g. stack trace.
+     * @param status The status to finish the test run with.
+     * @param msg    A short message explaining the result of the test run.
+     * @param detail Detailed information, e.g. a stack trace.
      */
     finish(status: TestRunEvaluationStatus, msg?: string, detail?: string): Observable<void> {
         return this.session.testMgmt.finishTestRun(this.id, status, msg, detail);
     }
 
     /**
-     * Block, until the TestRun goes into a finished state (completed or failed).
+     * Block until the test run goes into a finished state (completed or failed) or timeout occurs.
+     * The default timeout is 10 minutes.
+     * If the test run does not finish before the timeout, the returned observable will go into an error state.
      *
-     * @return returns the TestRun info of the finished TestRun.
+     * @return The test run info of the finished test run.
      */
     waitForCompletion(): Observable<TestRunInfo> {
-        return of("").pipe(
-            mergeMap(() => this.retrieveCurrentInfo()),
-            map(info => {
-                if (info.executionStatus == TestRunExecutionStatus.RUNNING || info.executionStatus == TestRunExecutionStatus.CREATED
-                    || info.evaluationStatus == TestRunEvaluationStatus.PENDING_PASSED || info.evaluationStatus == TestRunEvaluationStatus.PENDING_FAILED) {
-                    throw new Error(`Could not get test run info within timeout.`);
-                } else {
-                    return info;
-                }
-            }),
-            retryWhen(errors => concat(errors.pipe(delay(this.WAITING_POLLINTERVAL_MILLIS), take(this.MAX_RETRIES)), throwError(errors)))
-        );
+        return this.session.testMgmt.waitForTestRunCompletion(this.id);
     }
 }
